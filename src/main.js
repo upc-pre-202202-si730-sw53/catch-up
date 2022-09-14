a9aab1ea1a85d11d6c6cd1f671eb3186dc3f9daf
Angel Velasquez <pcisavel@upc.edu.pe>
2022-09-14 10:23:37 -0500

    chore: Added needed UI Components.

--- src/main.js
+++ src/main.js
@@ -15,8 +15,26 @@ import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
 import i18n from "@/i18n";
 
+// Add UI Components
+import Card from "primevue/card";
+import Button from "primevue/button";
+import SelectButton from "primevue/selectbutton";
+import Sidebar from "primevue/sidebar";
+import Avatar from "primevue/avatar";
+import Menu from "primevue/menu";
+import Menubar from "primevue/menubar";
+import Toolbar from "primevue/toolbar";
+
 
 createApp(App)
     .use(i18n)
     .use(PrimeVue, { ripple: true })
-    .mount('#app')
+    .component('pv-card', Card)
+    .component('pv-button', Button)
+    .component('pv-select-button', SelectButton)
+    .component('pv-sidebar', Sidebar)
+    .component('pv-avatar', Avatar)
+    .component('pv-menu', Menu)
+    .component('pv-menubar', Menubar)
+    .component('pv-toolbar', Toolbar)
+    .mount('#app');
