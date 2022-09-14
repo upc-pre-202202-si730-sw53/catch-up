f33f405bd2955b8f10d27b50de60858ff03d16cb
Angel Velasquez <pcisavel@upc.edu.pe>
2022-09-14 10:48:41 -0500

    feat: Added article list visualization and sidebar rendering.

--- src/components/side-menu.component.vue
+++ src/components/side-menu.component.vue
@@ -1,13 +1,29 @@
 <template>
-
+  <pv-sidebar v-model:visible="visible">
+    <div v-for="source in sources" class="m-4">
+      <div @click="onSourceSelected(source)"
+           class="flex align-content-start flex-wrap">
+        <span class="flex align-items-center justify-content-center mr-2">
+          <p-avatar :image="source.urlToLogo"
+                    shape="circle"
+                    :aria-label="source.name"></p-avatar>
+        </span>
+        <span class="flex align-items-center justify-content-center">
+          {{ source.name }}
+        </span>
+      </div>
+    </div>
+  </pv-sidebar>
 </template>
 
 <script>
-import { NewsApiService } from "@/news/services/news-api.service";
+import {NewsApiService} from "@/news/services/news-api.service";
 
 export default {
   name: "side-menu",
-
+  props: {
+    visible: Boolean
+  },
   data() {
     return {
       sources: [],
